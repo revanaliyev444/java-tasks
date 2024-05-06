a09b2fb2252266f27b1c14b19a0ff28e5333f18d
revanaliyev444 <revan1aliyev66@gmail.com>
2024-05-06 12:25:14 +0400

    init accepted

--- src/main/java/paymentSystem/Card.java
+++ src/main/java/paymentSystem/Card.java
@@ -1,2 +1,30 @@
-package paymentSystem;public class Card {
+package paymentSystem;
+
+import java.util.Scanner;
+
+public class Card implements Payment {
+    @Override
+    public void pay() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a card number: ");
+        String num = scanner.nextLine();
+
+        boolean isCorrect = true;
+
+        if (num.length() != 8) {
+            isCorrect = false;
+        } else {
+            for (int i = 0; i < num.length(); i++) {
+                if (!Character.isDigit(num.charAt(i))) {
+                    isCorrect = false;
+                    break;
+                }
+            }
+        }
+        if (!isCorrect) {
+            System.out.println("It's not correct card number");
+        } else {
+            System.out.println("Operation is correct");
+        }
+    }
 }
