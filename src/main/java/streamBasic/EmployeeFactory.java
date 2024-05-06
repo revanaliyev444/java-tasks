0e13eba5afebe620e421a0c1138553fabd354918
revanaliyev444 <revan1aliyev66@gmail.com>
2024-05-06 12:17:24 +0400

    init accepted after changes

--- src/main/java/streamBasic/EmployeeFactory.java
+++ src/main/java/streamBasic/EmployeeFactory.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class EmployeeList {
+public class EmployeeFactory {
     ArrayList<Employee> employees = new ArrayList<>();
 
     public List<Employee> getAllEmployee(){
